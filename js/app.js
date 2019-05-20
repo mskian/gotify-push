f0c20fa9043b0d32df9ab01aca35a5915f9c7387
Harish Kumar <harishkumar.p24@gmail.com>
2019-05-20 13:23:41 +0530

    👌 IMPROVE: Push notiofication page JS improvements

--- js/app.js
+++ js/app.js
@@ -5,40 +5,43 @@ chrome.storage.sync.get({
 });
 
 function cleanupFeed(apiurl) {
-    var el = document.getElementById('postData');
+    var el = document.querySelector('#postData');
     if (el) {
         el.addEventListener('submit', postData);
     }
 
     function postData(event) {
         event.preventDefault();
-        let title = document.getElementById('title').value;
-        let message = document.getElementById('message').value;
+        let title = document.querySelector('#title').value;
+        let message = document.querySelector('#message').value;
         var data = {
             title: title,
             message: message,
             priority: 5
         };
 
-        const send = document.getElementById('push');
-        send.textContent = 'Pushed SuccessFully';
-        setTimeout(() => {
-            send.textContent = 'Refreshing the Page for Next Push';
-        }, 1000);
-        setTimeout(() => {
-            window.location.reload();
-        }, 3000);
+        const send = document.querySelector('#push');
+        send.classList.add("loading")     
 
         fetch(apiurl, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            })
-            .then(res => res.json())
-            .then(response => console.log('Success:', JSON.stringify(response)))
-            .catch(error => console.error('Error:', error));
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(
+            el.reset(),
+            res => res.json(),
+            setTimeout(() => {
+                send.classList.remove("loading")
+                send.textContent = "Pushed!"
+            }, 500),
+            setTimeout(() => {
+                send.textContent = "Send"
+            }, 700)
+        )
+        .then(response => console.log('Success:', JSON.stringify(response)))
+        .catch(error => console.error('Error:', error));
     }
-
 }
